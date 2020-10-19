6704dd29a439731b15a3734b812400dad9e4ea95
Adam Kolterud <adam.kolterud@soprasteria.com>
2020-10-19 16:45:29 +0200

    addedtestpkg

---
+++ ClassLibrary/Hello.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Hello
+    {
+        private string Greeting;
+
+        public Hello(string greeting)
+        {
+            this.Greeting = greeting;
+
+        }
+
+        public void sayHello()
+        {
+            Console.WriteLine(Greeting);
+        }
+    }
+}
